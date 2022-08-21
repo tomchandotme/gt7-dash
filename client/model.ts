755329092f58aea2807a332ca2af6471c884b87e
tomchan <tomchan00727@gmail.com>
2022-08-21 18:01:35 +0800

    client: updated model

--- client/model.ts
+++ client/model.ts
@@ -17,6 +17,47 @@ type Vector3Rotation = {
   roll: number;
 };
 
+enum Flags {
+  None = 0,
+
+  /// The car is on the track or paddock, with data available.
+  CarOnTrack = 1 << 0,
+
+  /// The game's simulation is paused.
+  /// Note: The simulation will not be paused while in the pause menu in online modes.
+  Paused = 1 << 1,
+
+  /// Track or car is currently being loaded onto the track.
+  LoadingOrProcessing = 1 << 2,
+
+  /// Needs more investigation
+  InGear = 1 << 3,
+
+  /// Current car has a Turbo.
+  HasTurbo = 1 << 4,
+
+  /// Rev Limiting is active.
+  RevLimiterBlinkAlertActive = 1 << 5,
+
+  /// Hand Brake is active.
+  HandBrakeActive = 1 << 6,
+
+  /// Lights are active.
+  LightsActive = 1 << 7,
+
+  /// High Beams are turned on.
+  HighBeamActive = 1 << 8,
+
+  /// Low Beams are turned on.
+  LowBeamActive = 1 << 9,
+
+  /// ASM is active.
+  ASMActive = 1 << 10,
+
+  /// Traction Control is active.
+  TCSActive = 1 << 11,
+}
+
 export type SimulatorPacket = {
   magic: 1194808112;
   position: Vector3;
@@ -45,7 +86,7 @@ export type SimulatorPacket = {
   minAlertRPM: number;
   maxAlertRPM: number;
   calculatedMaxSpeed: number;
-  flags: number;
+  flags: Flags;
   currentGear: number;
   suggestedGear: number;
   throttle: number;
